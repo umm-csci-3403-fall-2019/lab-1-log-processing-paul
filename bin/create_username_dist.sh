7da0379dd7e93bc6f9eeca4f6a0a41ba64c73651
Paul Friederichsen <floogulinc@gmail.com>
2019-09-06 16:18:42 -0500

    Fix username matching
    I wasn't matching '-' in usernames

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -9,6 +9,6 @@
 
 cat html_components/username_dist_header.html > "${1}"/username_dist.html
 
-awk '{ print $4 }' "${1}"/*/failed_login_data.txt | sort | uniq -c | awk 'match($0, /([0-9]+) ([A-Za-z0-9_]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' >> "${1}"/username_dist.html
+awk '{ print $4 }' "${1}"/*/failed_login_data.txt | sort | uniq -c | awk 'match($0, /([0-9]+) ([A-Za-z0-9_-]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' >> "${1}"/username_dist.html
 
 cat html_components/username_dist_footer.html >> "${1}"/username_dist.html
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,4 +2,4 @@
 
 cd "$1"
 
-awk 'match($0, /^([A-Za-z]+)[ ]+([0-9]+ [0-9][0-9]):[0-9:]+ [A-Za-z]+ sshd\[[0-9]+\]: Failed password for[A-Za-z0-9 ]* ([A-Za-z0-9_]+) from ([0-9]+.[0-9]+.[0-9]+.[0-9]+)/, groups) {print groups[1], groups[2], groups[3], groups[4]}' var/log/* > failed_login_data.txt
+awk 'match($0, /^([A-Za-z]+)[ ]+([0-9]+ [0-9][0-9]):[0-9:]+ [A-Za-z]+ sshd\[[0-9]+\]: Failed password for[A-Za-z0-9 ]* ([A-Za-z0-9_-]+) from ([0-9]+.[0-9]+.[0-9]+.[0-9]+)/, groups) {print groups[1], groups[2], groups[3], groups[4]}' var/log/* > failed_login_data.txt
