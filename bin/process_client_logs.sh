8f5445434c45c61df2d4ad75b960ed1366ab50ad
Paul Friederichsen <floogulinc@gmail.com>
2019-09-10 09:58:52 -0500

    Add "|| exit 1" to cd in process_client_logs.sh
    Prevents the script from continuing in the event the cd fails.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd "$1"
+cd "$1" || exit 1
 
 awk 'match($0, /^([A-Za-z]+)[ ]+([0-9]+ [0-9][0-9]):[0-9:]+ [A-Za-z]+ sshd\[[0-9]+\]: Failed password for[A-Za-z0-9 ]* ([A-Za-z0-9_-]+) from ([0-9]+.[0-9]+.[0-9]+.[0-9]+)/, groups) {print groups[1], groups[2], groups[3], groups[4]}' var/log/* > failed_login_data.txt
